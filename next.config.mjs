b64620cd55ab98247b9965666f6c84cec09a620d
theanishtar <89396036+dangtranhuu@users.noreply.github.com>
2025-04-12 09:24:34 +0700

    ci: update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-import 'dotenv/config'; // Giúp load biến môi trường từ .env
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? `/${process.env.REPO_NAME}` : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${process.env.REPO_NAME}/` : '',
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
