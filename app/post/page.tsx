ddd566e0de08e2e003aa44ebb2b96eb58a65449c
theanishtar <89396036+dangtranhuu@users.noreply.github.com>
2025-04-18 15:17:33 +0700

    fix: rename Page.tsx to page.tsx to fix casing issue on case-sensitive systems

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,5 +1,5 @@
 // app/page.tsx hoặc app/post/page.tsx (tuỳ bạn)
-import PostListServer from './PostListServer'
+import PostListServer from './postListServer1'
 
 export default function Page() {
   return <PostListServer />
